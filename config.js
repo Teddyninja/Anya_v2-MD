4d9054cdffecd369054de3edc2062cd3af2780a8
Teddyninja <165490902+Teddyninja@users.noreply.github.com>
2024-08-16 21:39:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    sessionId: process.env.SESSION_ID || "aUFJbHlZSlojc01mVGQ_Queen-Anya_wdTVqV2o1NDhfYjJRQ_Queen-Anya_UhmOGgwVXdjOWt0ZEhDclRPbGoyeUtHZw==",
 
     /**
      * thumbnails and medias
